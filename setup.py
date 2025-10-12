1bb429aa7c85f993fa8b36dc767329c7fb8e52a5
DaviDev <97841570+DaviisDev@users.noreply.github.com>
2025-10-12 13:38:17 -0300

    + +

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import setup, Extension, find_packages
+
+extModules = [
+    Extension(
+        "tgcrypto",
+        sources=[
+            "tgcrypto/tgcrypto.c",
+            "tgcrypto/aes256.c",
+            "tgcrypto/ige256.c",
+            "tgcrypto/ctr256.c",
+            "tgcrypto/cbc256.c"
+        ],
+    )
+]
 
 setup(
-    packages=find_packages()
+    packages=find_packages(),
+    ext_modules=extModules
 )
